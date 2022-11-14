5024428a35a10028f462faf7b943eaf6bc58815b
RoffanRayok <roffanrayok124@gmail.com>
2022-11-14 18:57:36 +1300

    Program

---
+++ June 2022/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+IWebDriver driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl("http://localhost:5000/Home");
+
+IWebElement SigininTextbox = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+SigininTextbox.Click();
+
+IWebElement EmailTextbox = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[1]/input"));
+EmailTextbox.SendKeys("rayokroffan@gmail.com");
+
+IWebElement PasswordTextbox = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[2]/input"));
+PasswordTextbox.SendKeys("TestAnalyst2023");
+
+IWebElement LoginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
+LoginButton.Click();
+
+IWebElement HiRoffan = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/div[1]/div[2]/div/span"));
+
+if (HiRoffan.Text == "HiRoffan")
+{
+    Console.WriteLine("Logged in successfully, test passed");
+
+}
+else
+{
+    Console.WriteLine("Login Failed, test failed");
+
+}
---
+++ Test Analyst July 2022/MarsTesting.cs
@@ -0,0 +1,29 @@
+﻿using Docker.DotNet.Models;
+using java.sql;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Analyst_July_2022
+{ 
+    public class CommonDriver 
+
+       
+    //open chrome browser
+    IWebDriver driver = new ChromeDriver();
+    
+    driver.Navigate().GoToUrl("http://localhost:5000/Home");
+    
+
+    IWebElement signinTextbox = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+
+
+
+
+    
+
